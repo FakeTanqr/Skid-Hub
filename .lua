8b2fb6fd8005ec53718c75b0b2d68c7e0ab4d25e
PetSimXScriots <96972490+FakeTanqr@users.noreply.github.com>
2024-04-02 12:42:24 +0800

    Skid Hub.lua

---
+++ .lua
@@ -0,0 +1,34 @@
+--[[
+__________                   __               __   ProjectV Hub
+\______   \_______  ____    |__| ____   _____/  |_  \       /
+ |     ___/\_  __ \/  _ \   |  |/ __ \_/ ___\   __\  \     / 
+|    |     |  | \(  <_> )  |  \  ___/\  \___|  |      \   /
+ |____|     |__|   \____/\__|  |\___  >\___  >__|      \ /
+                        \______|    \/     \/               
+]]--
+
+
+
+
+local ID = game.PlaceId
+if ID == 7346416636 then
+    warn("Pop It Trading has been Loaded!")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FakeTanqr/ScriptPopItDupe/main/Script.lua"))()
+elseif ID == 16489337548 then
+    print("Aura RNG has been Loaded!")
+    loadstring(game:HttpGet("https://pastebin.com/raw/AbUWbHx2"))()
+elseif ID == 16556777270 then
+    print("Hade's RNG has been Loaded!")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LOLking123456/hades/main/rng"))()
+elseif ID == 15367026228 then
+    print("Anime Souls X has been Loaded!")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Vmax0/RobloxScripts/main/Scripts/AnimeSoulsX.lua"))()
+elseif ID == 13885546444 then
+    print("Mowing Simulator has been Loaded!")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Vmax0/RobloxScripts/main/Scripts/MowingSimulator.lua"))()
+elseif ID == 8216162783 then
+    print("Cars Trading has been Loaded!")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Vmax0/RobloxScripts/main/Scripts/CarsTrading.lua"))()
+else
+    print("Unknown PlaceId:", ID)
+end
